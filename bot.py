8d77c97db9792dd42685eb2f3f94bedee46dabd9
sensecherise <sensecherise@gmail.com>
2018-08-23 10:58:05 +0700

    update af

--- bot.py
+++ bot.py
@@ -15,19 +15,19 @@ def hello():
 @app.route("/webhook", methods=['GET','POST'])
 def webhook():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+   # signature = request.headers['X-Line-Signature']
 
     # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+   # body = request.get_data(as_text=True)
+   # app.logger.info("Request body: " + body)
 
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
+  #  try:
+  #      handler.handle(body, signature)
+   # except InvalidSignatureError:
+   #     abort(400)
+    if request.method == 'POST':
+        return 'OK'
     
 
 @handler.add(MessageEvent, message=TextMessage)
