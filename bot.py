b9a921f2cbafac81070c258f4ca9378183177327
sensecherise <sensecherise@gmail.com>
2018-08-24 17:21:56 +0700

    dd

--- bot.py
+++ bot.py
@@ -69,12 +69,14 @@ def handle_text_message(event):
             )
     elif event.message.text == '!checkMonitor':
         response = requests.get(url+'Monitor/CheckAppsStatus')
-        response.json()
+        r = response.json()
+        response = response.json()
+        print(r)
         print(response)
         if isinstance(event.source, SourceGroup):
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text= response.BotMessage)
+                TextSendMessage(text= r.BotMessage)
             )
     else:
         if isinstance(event.source, SourceUser):
