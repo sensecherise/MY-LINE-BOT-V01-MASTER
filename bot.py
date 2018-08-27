31479c809b9cfa9dbe6dfac738763e2844f584cd
sensecherise <sensecherise@gmail.com>
2018-08-27 10:45:01 +0700

    s

--- bot.py
+++ bot.py
@@ -70,6 +70,7 @@ def handle_text_message(event):
             )
     elif event.message.text == '!checkMonitor':
         response = requests.get(url+'Monitor/CheckAppsStatus')
+        print(response)
         # r = response.json()
         # print(type(r))
         # print(r)
