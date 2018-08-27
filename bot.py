9758aa9c8115237e312139f35566dbc13ff3063c
sensecherise <sensecherise@gmail.com>
2018-08-27 13:16:41 +0700

    aaa

--- bot.py
+++ bot.py
@@ -95,12 +95,12 @@ def handle_text_message(event):
             )
 
 #test linegroup
-i = 0
-while i < 1000000:
-    line_bot_api.push_message('C8d731f5c4671277e13f9d49259281539', 
-    TextSendMessage(text=i+': Hello Why'))
-    i = i + 1
-    time.sleep(30)
+# i = 0
+# while i < 1000000:
+#     line_bot_api.push_message('C8d731f5c4671277e13f9d49259281539', 
+#     TextSendMessage(text=i+': Hello Why'))
+#     i = i + 1
+#     time.sleep(30)
     
 
 #dev linegroup C66e0fc5a2d23607150d592d8396f4832
