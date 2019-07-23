a5b37a0364fdff32e4e40a904f30829ceab45d99
sensecherise <sensecherise@gmail.com>
2019-07-23 14:16:38 +0700

    update fork

--- bot.py
+++ bot.py
@@ -23,20 +23,20 @@ now = datetime.datetime.now()
 url = 'https://its.rvp.co.th/it/api/'
 
 #chappie
-#line_bot_api = LineBotApi('Py16F9GZePWwoBlq/r7aev30s9SUMYPsP9YAQpr4XBE2skelccadOvgDO8D04HMdNgmrVpu/N0edN8uBHVR36XlErDXbRFW2ODlr2yppKwSbKbhTTIPWe0sek7pzlvwySvDx04TSiPTTJDQAMrYjjgdB04t89/1O/w1cDnyilFU=')
+accesstoken = 'Py16F9GZePWwoBlq/r7aev30s9SUMYPsP9YAQpr4XBE2skelccadOvgDO8D04HMdNgmrVpu/N0edN8uBHVR36XlErDXbRFW2ODlr2yppKwSbKbhTTIPWe0sek7pzlvwySvDx04TSiPTTJDQAMrYjjgdB04t89/1O/w1cDnyilFU='
 #khun_sinmai
 #line_bot_api = LineBotApi('B7+XD/E492NrBIx9nHNpOwBdTbQ9ZNoayiOKD1ZLZvOqE5QATL9fNzKLy2NyvN0wHpmw3MmZDDj+8N81w8ckhLeE5/Fou1RW5ngnVJQQGlGg2XpP5nptvR1+jYFH2bYOnf5nR/2wJ5RkghN0It2n1QdB04t89/1O/w1cDnyilFU=')
 #rvp
-accesstoken = 'x+slzRC7b27fT1CbP0n1W4jsfniVlpWaw2hHDrUiBhbLxZ9eNqcmsplXgO4G1FsHLWPcotN8F6uysU4yAHXS+0G4+rQul2Tnli+ea/HvB+l8HVnoN9SEZyvXqxDVj7nDxUffzPKKJTzqdBXMZ5qsUwdB04t89/1O/w1cDnyilFU='
+# accesstoken = 'x+slzRC7b27fT1CbP0n1W4jsfniVlpWaw2hHDrUiBhbLxZ9eNqcmsplXgO4G1FsHLWPcotN8F6uysU4yAHXS+0G4+rQul2Tnli+ea/HvB+l8HVnoN9SEZyvXqxDVj7nDxUffzPKKJTzqdBXMZ5qsUwdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(accesstoken)
 
 
 #chappie
-#handler = WebhookHandler('63bd28fd3a4fdadaa9655de644902fcc')
+channelsecret = '63bd28fd3a4fdadaa9655de644902fcc'
 #khun_sinmai
 #handler = WebhookHandler('1271b23fb3f7217041dcaa37548f7bdd')
 #rvp
-channelsecret = 'f375e3fce9c1ac3548f0f40d4ef8238d'
+# channelsecret = 'f375e3fce9c1ac3548f0f40d4ef8238d'
 handler = WebhookHandler(channelsecret)
 
 bottestgroup = 'Cbe8afa803412d3558dffcc9a1a8c4a71'
@@ -281,7 +281,7 @@ def sendDbjobData(group_line):
         return False
 
 try:
-    groupLine = selfid
+    groupLine = monitorgroup
     appCheck = sendAppData(groupLine)
     dbCheck = sendDbjobData(groupLine)
     while(appCheck == False or dbCheck == False):
